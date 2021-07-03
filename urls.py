80bbddcd49ac194ff3e20d889af74a36c3287ef5
Claudio Tubertini <claudio.tubertini@gmail.com>
2021-07-03 16:31:28 +0200

    commit

--- urls.py
+++ urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from graphs import views
 
 urlpatterns = [
-    path('', views.graphs, name='graphs'),
+    # path('', views.graphs, name='graphs'),
     path('population-chart', views.population_chart, name='population-chart'),
+    path('', views.dashboard_with_pivot, name='dashboard_with_pivot'),
+    # path('data', views.pivot_data, name='pivot_data'),
 ]
--- views.py
+++ views.py
@@ -15,23 +15,41 @@ import public_site.confirmed_adoptions
 import public_site.streamcsv
 from django.contrib.auth import authenticate, login
 from django.db import connection
+from django.core import serializers
+import json
 
 
 def graphs(request):
-    return render(request, 'graph_D3.html')
-    
+    return render(request, 'graph_D3_2.html', {})
 
+### oppure ###
+
+def dashboard_with_pivot(request):
+    return render(request, 'dashboard_with_pivot.html', {})
 
 def population_chart(request):
     data = public_site.queries.search_test()
+    # data = serializers.serialize('json', data, fields=('publisher', 'count'))
+    result = []
+    keys = ('publisher','count')
+    for row in data:
+        result.append(dict(zip(keys,row)))
+    #json_data = json.dumps(result)
+    return JsonResponse(result, safe=False)
+    # publisher = []
+    # count = []
+    # for entry in data:
+    #     publisher.append(entry[0])
+    #     count.append(entry[1])
     
-    publisher = []
-    count = []
-    for entry in data:
-        publisher.append(entry[0])
-        count.append(entry[1])
-    
-    return JsonResponse(data={
-        'publisher': publisher,
-        'count': count,
-    })
+    # return JsonResponse(data={
+    #     'publisher': publisher,
+    #     'count': count,
+    # })
+
+## oppure ###
+def pivot_data(request):
+    data = public_site.queries.search_test()
+    # dataset = Order.objects.all()
+    data = serializers.serialize('json', data)
+    return JsonResponse(data, safe=False)
