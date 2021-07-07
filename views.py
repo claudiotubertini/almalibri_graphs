b291d056ae8546cd31d25850e13c1d08be32bdd7
Claudio Tubertini <claudio.tubertini@gmail.com>
2021-07-07 09:56:23 +0200

    quali grafici

--- views.py
+++ views.py
@@ -15,15 +15,16 @@ import public_site.streamcsv
 from django.contrib.auth import authenticate, login
 from django.db import connection
 from django.core import serializers
-import json
+import json, csv
 import pandas as pd
 
 def handle_uploaded_file(f):
     result = []
-    with open('area16_sps6.csv', newline='') as csvfile:
+    with open(f, newline='') as csvfile:
          reader = csv.DictReader(csvfile, delimiter=';')
          for row in reader:
             result.append(row)
+    return result
     # df = pd.read_csv (f)
     # df.to_json (r'Path where the new JSON file will be stored\New File Name.json')
     # with open('some/file/name.txt', 'wb+') as destination:
@@ -34,15 +35,15 @@ def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
-            return HttpResponseRedirect('/success/url/')
+            data = handle_uploaded_file(request.FILES['file'])
     else:
         form = UploadFileForm()
+    #return JsonResponse(data, safe=False)
     return render(request, 'upload.html', {'form': form})
 
 
 def graphs(request):
-    return render(request, 'graph_D3_2.html', {})
+    return render(request, 'graph_D3_A.html')
 
 ### oppure ###
 
@@ -50,8 +51,9 @@ def dashboard_with_pivot(request):
     return render(request, 'dashboard_with_pivot.html', {})
 
 def population_chart(request):
-    data = public_site.queries.search_test()
+    #data = public_site.queries.search_test()
     # data = serializers.serialize('json', data, fields=('publisher', 'count'))
+    result = upload_file(request)
     result = []
     keys = ('publisher','count')
     for row in data:
